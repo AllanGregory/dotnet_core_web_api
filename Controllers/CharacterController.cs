3871c7d27cd31dbc2985f633799ba37b7b5f4e72
Allan Gregory <alngregory@gmail.com>
2020-05-10 17:03:04 -0300

    Aula 02 - implementando classe services e fazendo injeção de dependência

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using dotnet_rpg.Models;
+using System.Collections.Generic;
+using System.Linq;
+using dotnet_rpg.Services;
 
 namespace dotnet_rpg.Controllers
 {
@@ -7,11 +10,38 @@ namespace dotnet_rpg.Controllers
     [Route("[controller]")]
     public class CharacterController : ControllerBase
     {
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+
+        }
+
         private static Character rogue = new Character();
 
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character { Id = 1, Name = "Sam" }
+        };
+
+        [HttpGet("GetAll")]
         public IActionResult Get()
         {
-            return Ok(rogue);
+            return Ok(_characterService.GetAllCharacters());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(_characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public IActionResult AddCharacter(Character newCharacter)
+        {
+            return Ok(_characterService.AddCharacter(newCharacter));
         }
     }
 }
